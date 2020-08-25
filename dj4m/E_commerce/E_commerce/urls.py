0754f9f3f1fff845af59b87ffdf8163768f74f2f
NazmulHasanMoon <nazmulmoon123@gmail.com>
2020-08-25 12:11:05 +0600

    Blog files

--- dj4m/E_commerce/E_commerce/urls.py
+++ dj4m/E_commerce/E_commerce/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('',views.index,name='Home'),
+	path('',include("shop.urls")),
 	path('shop/',include("shop.urls")),
 	path('blog/',include("blog.urls")),
 ]
