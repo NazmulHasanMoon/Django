90947087f130afc8174ba1357d344aaf51ed8127
NazmulHasanMoon <nazmulmoon123@gmail.com>
2020-09-13 18:22:41 +0600

    Update bloging/urls.py to change admin sides web title

--- dj4m/bloging/bloging/urls.py
+++ dj4m/bloging/bloging/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
+admin.site.site_header="MoonPlanet Admin"
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('',include('home.urls')),
