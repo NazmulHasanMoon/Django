c4559e480fc9caaece3a73efc083ea99d6a9a3bf
NazmulHasanMoon <nazmulmoon123@gmail.com>
2020-10-01 09:58:25 +0600

    render data from Database with a Model

--- dj4m/trydjango/products/views.py
+++ dj4m/trydjango/products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Product
+
+def product_detail_view(request):
+	obj=Product.objects.get(id=1)
+	#context={
+	#	"title": obj.title,
+	#	"description" : obj.description,
+	#	"price" : obj.price
+	#}
+	context={
+		"object":obj
+	}
+	return render(request,'product/detail.html',context)
