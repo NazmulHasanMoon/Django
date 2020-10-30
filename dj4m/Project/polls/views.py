2a41aef7819ffa6e8b2de803eede6586f1825a4c
NazmulHasanMoon <nazmulmoon123@gmail.com>
2020-10-30 10:21:35 +0600

    add test cases and update polls admin

--- dj4m/Project/polls/views.py
+++ dj4m/Project/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question,Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 #def index(request):
 #	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -12,8 +13,9 @@ class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
 	context_object_name = 'latest_question_list'
 	def get_queryset(self):
-		"""Return the last five published questions."""
-		return Question.objects.order_by('-pub_date')[:5]
+		"""Return the last five published questions 
+		(not including those set to bepublished in the future)."""
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 #def detail(request, question_id):
 #	question = get_object_or_404(Question, pk=question_id)
@@ -21,15 +23,22 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
+	def get_queryset(self):
+		"""
+		Excludes any questions that aren't published yet.
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 #def results(request, question_id):
 #	response = "You're looking at the results of question %s."
 #	return HttpResponse(response % question_id)
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
-
+#def results(request, question_id):
+#	question = get_object_or_404(Question, pk=question_id)
+#	return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 #def vote(request, question_id):
 #	return HttpResponse("You're voting on question %s." % question_id)
 
